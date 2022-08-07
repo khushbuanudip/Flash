7608b20e2802dca9bc5b3b35aea4197f410d1248
Khushbu <Khushbu@DESKTOP-I3GP23C>
2022-08-07 16:59:21 +0530

    first commit

---
+++ operator/src/operator/mai.java
@@ -0,0 +1,18 @@
+package operator;
+import java.util.Scanner; 
+public class mai {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		if(20>18)
+		{
+		System.out.println("20 is greater than 18");
+		}
+		 
+		  }
+	
+		 
+	}
+
+
