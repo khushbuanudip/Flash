771ab5e0e465f642f69d37b3db2c1fa039c58e73
Khushbu <Khushbu@DESKTOP-I3GP23C>
2022-08-07 18:01:22 +0530

    commit

---
+++ ifelse/src/ifelse/main1.java
@@ -0,0 +1,157 @@
+package ifelse;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class main1 {  
+  public static void main(String[] args) { 
+	  
+	 int x =20;
+	  int y =10;
+	  x=6;
+	  if( x > y)
+	  {
+		  
+	  }
+	 x=12;
+	 System.out.println("Hello world");
+    } 
+  }  
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
