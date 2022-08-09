ec09b86e547a3dd05af5832a0e728d3b07c91fcc
Khushbu <Khushbu@DESKTOP-I3GP23C>
2022-08-09 14:24:25 +0530

    first commit

---
+++ Junittest/src/JunitTesting.java
@@ -0,0 +1,22 @@
+
+public class JunitTesting {
+	
+	public int square(int x)
+	{
+		return x*x;
+	}
+	public int countA(String word)
+	{
+		int count=0;
+		for (int i=0;i<word.length();i++)
+		{
+			if (word.charAt(i)=='a')
+			{
+				count++;
+			}
+			
+		}
+		return count;
+	}
+    
+}
