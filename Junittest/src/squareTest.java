c057fdb571a0e36ba82d9aa704543f468ada3ee8
Khushbu <Khushbu@DESKTOP-I3GP23C>
2022-08-13 11:39:42 +0530

    first commit

--- Junittest/src/squareTest.java
+++ Junittest/src/squareTest.java
@@ -1,15 +1,39 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
-
+import static java.lang.System.*;
 class squareTest {
+	@BeforeClass
+	public void m1()
+	{
+		out.print("this will execute befor all test");
+	}
+	@Before
+	void m2()
+	{
+		out.print("this will execute befor each test");
+	}
 
-	@Test
+	@Ignore
 	void test() {
-	JunitTesting test=new JunitTesting();
+		String str4="hello";
+		String str5="hello";
+		assertSame(str4,str5);
+		
+		JunitTesting test=new JunitTesting();
 	int output=test.square(5);
 	assertEquals(24,output);
-			
+	
 	}
+	@Test
+	void test1() {
+		JunitTesting test=new JunitTesting();
+	int count1=test.countA("apple orange")	;
+	assertEquals(count1,2);
+	}
+	
 
 }
